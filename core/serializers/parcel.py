6df48fc8b618ee2607e4fc517327011b6e488771
stephenprn <stephen.perrin@2017.icam.fr>
2024-07-23 17:20:26 +0200

    improve PDF signalement

--- core/serializers/parcel.py
+++ core/serializers/parcel.py
@@ -1,4 +1,6 @@
+from core.contants.order_by import GEO_CUSTOM_ZONES_ORDER_BYS
 from core.models.detection_object import DetectionObject
+from core.models.geo_custom_zone import GeoCustomZone, GeoCustomZoneStatus
 from core.models.parcel import Parcel
 from core.serializers import UuidTimestampedModelSerializerMixin
 from core.serializers.detection import DetectionWithTileSerializer
@@ -6,6 +8,8 @@ from core.serializers.geo_commune import GeoCommuneSerializer
 from core.serializers.detection_object import DetectionObjectMinimalSerializer
 
 from rest_framework import serializers
+
+from core.serializers.geo_custom_zone import GeoCustomZoneSerializer
 
 
 class ParcelMinimalSerializer(UuidTimestampedModelSerializerMixin):
@@ -54,6 +58,29 @@ class ParcelDetectionObjectSerializer(DetectionObjectMinimalSerializer):
 
 class ParcelDetailSerializer(ParcelSerializer):
     class Meta(ParcelSerializer.Meta):
-        fields = ParcelSerializer.Meta.fields + ["detection_objects"]
+        fields = ParcelSerializer.Meta.fields + [
+            "detection_objects",
+            "custom_geo_zones",
+        ]
 
     detection_objects = ParcelDetectionObjectSerializer(many=True)
+    custom_geo_zones = serializers.SerializerMethodField()
+
+    def get_custom_geo_zones(self, obj: Parcel):
+        geo_custom_zones_data = GeoCustomZone.objects.order_by(
+            *GEO_CUSTOM_ZONES_ORDER_BYS
+        ).filter(geo_custom_zone_status=GeoCustomZoneStatus.ACTIVE)
+        geo_custom_zones_data = geo_custom_zones_data.filter(
+            geometry__intersects=obj.geometry
+        )
+        geo_custom_zones_data = geo_custom_zones_data.values(
+            "uuid", "name", "color", "geo_custom_zone_status"
+        )
+        geo_custom_zones_data = geo_custom_zones_data.all()
+
+        geo_custom_zones = []
+
+        for geo_custom_zone in geo_custom_zones_data:
+            geo_custom_zones.append(GeoCustomZoneSerializer(geo_custom_zone).data)
+
+        return geo_custom_zones
