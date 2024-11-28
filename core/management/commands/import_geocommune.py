1afae3a73e9403a4aef3bf9f15a4c63b0e634384
stephenprn <stephen.perrin@2017.icam.fr>
2024-11-28 16:40:40 +0100

    improve import_geocommune cmd

--- core/management/commands/import_geocommune.py
+++ core/management/commands/import_geocommune.py
@@ -1,6 +1,7 @@
 import json
 from typing import Dict, List, TypedDict
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 
 from core.management.commands._common.file import (
     download_json,
@@ -24,17 +25,25 @@ class CommuneProperties(TypedDict):
 class Command(BaseCommand):
     help = "Import communes to database from JSON"
 
+    def add_arguments(self, parser):
+        parser.add_argument("--dpt-insee-codes", action="append", required=False)
+
     def handle(self, *args, **options):
+        dpt_insee_codes = options["dpt_insee_codes"]
         data_communes: List[CommuneProperties] = download_json(FILE_JSON_URL)
 
         departments = GeoDepartment.objects.filter(
             insee_code__in=[
-                data_commune["dep_code"][0] for data_commune in data_communes
+                data_commune["dep_code"][0]
+                for data_commune in data_communes
+                if not dpt_insee_codes or data_commune["dep_code"][0] in dpt_insee_codes
             ]
         )
         department_code_department_map = {
             department.insee_code: department for department in departments
         }
+        print(f"Starting communes import for departments: {
+              ", ".join([dpt.name for dpt in departments])}")
 
         for data_commune in data_communes:
             name = data_commune["com_name"][0]
@@ -45,9 +54,6 @@ class Command(BaseCommand):
             department = department_code_department_map.get(department_code)
 
             if not department:
-                print(
-                    f"Department not found for code: {department_code}, skipping commune: {name}"
-                )
                 continue
 
             commune = GeoCommune(
@@ -56,4 +62,7 @@ class Command(BaseCommand):
                 geometry=geometry,
                 department=department,
             )
-            commune.save()
+            try:
+                commune.save()
+            except IntegrityError:
+                pass
